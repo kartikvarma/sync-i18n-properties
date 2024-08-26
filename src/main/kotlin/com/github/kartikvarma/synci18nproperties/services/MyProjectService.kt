1c0a32918592a9f53404aac4d136d41e486f8250
GitHub Action <action@github.com>
2024-08-26 16:38:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/kartikvarma/synci18nproperties/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kartikvarma/synci18nproperties/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.kartikvarma.synci18nproperties.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kartikvarma.synci18nproperties.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
